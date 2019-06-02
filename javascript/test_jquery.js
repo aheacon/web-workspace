31025acb0a2104ca0ee0007c485594a67687299a
Anel Husakovic <anelhusakovic@gmail.com>
2019-06-02 08:49:53 -0700

    Remove tracked files and start again

--- javascript/test_jquery.js
+++
@@ -1,8 +0,0 @@
-// Unlike vanilla js
-// $ is working as querySelectorAll()
-$("li");
-$('#submit').click(function(){
-	console.log("Click");
-	$('#table').append("<tr> <td scope='row'> Third1 </td> </tr>");
-	//$('#table > tbody:last-child').append('<tr>...</tr><tr>...</tr>');
-});
--- webapps/upis/tehnicka/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TehnickaConfig(AppConfig):
-    name = 'tehnicka'
